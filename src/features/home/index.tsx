1d9a38df96b486f44a2ffb1a07fcd67e6c21d646
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-15 01:04:33 +0600

    working on hero section of the home page

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -3,22 +3,22 @@ import { urbanist } from "@/fonts/fonts";
 
 const HomePage = () => {
   return (
-    <div className="container grid grid-cols-2">
-      <div>
-        <h2 className={`${urbanist.className} text-4xl font-bold`}>
+    <div className="container grid grid-cols-1 md:grid-cols-2">
+      <div className="order-2 md:order-1">
+        <h2 className={`${urbanist.className} text-2xl md:text-4xl font-extrabold md:font-bold text-center md:text-start`}>
           Want to Turn Social Media Into a Profitable Career?
         </h2>
-        <h2
+        <p
           style={{
             color: "#00E7F9",
             textShadow: "0px 4px 4px #FC004E",
           }}
-          className={`${urbanist.className} text-4xl font-bold text-[#00E7F9]`}
+          className={`${urbanist.className} text-4xl font-bold text-center md:text-start`}
         >
           Discover your way to success with Fametonic:
-        </h2>
+        </p>
       </div>
-      <div className="w-full h-[400px] bg-gray-500">
+      <div className="order-1 md:order-2 w-full h-[400px] bg-gray-500">
         <p>image</p>
       </div>
     </div>
