8f716021fb832c8a2af5ebf93aec64b474d2a6fe
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-15 22:33:27 +0600

    added button shadow

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { urbanist,nunito } from "@/fonts/fonts";
+import { urbanist, nunito } from "@/fonts/fonts";
 import Image from "next/image";
 import { descData } from "@/data/heroDescData";
 
@@ -17,7 +17,7 @@ const HomePage = () => {
             <p
               style={{
                 color: "#00E7F9",
-                textShadow: "0px 4px 4px #FC004E",
+                textShadow: "0px 4px 4px 0px #FC004E",
               }}
               className={`${urbanist.className} text-2xl md:text-4xl font-extrabold md:font-bold text-center md:text-start max-w-[21rem] md:max-w-full mx-auto md:mx-0`}
             >
@@ -35,26 +35,36 @@ const HomePage = () => {
             ))}
           </div>
           <div className="mt-5 space-y-6 flex flex-col">
-            <div className="order-2 md:order-1 w-full md:w-[313px] mt-6 md:mt-0">
-              <button className="bg-[#FC004E] text-white text-xl font-bold h-[40px] w-full flex justify-center items-center gap-1 rounded-[10px] cursor-pointer">
+            <div className="order-2 md:order-1 w-full md:w-[313px] mt-5 md:mt-0">
+              <button
+                style={{
+                  background: "#FC004E",
+                  boxShadow: "2px 2px 10px 0px #00E7F9",
+                }}
+                className="bg-[#FC004E] text-white text-xl font-bold h-[40px] w-full flex justify-center items-center gap-1 rounded-[10px] cursor-pointer"
+              >
                 GET STARTED
                 <span>
                   <img src="/icons/right.svg" alt="arrow" className="w-3 h-3" />
                 </span>
               </button>
-              <p className="mt-2 text-center font-normal text-xs">
+              <p className="mt-3 text-center font-normal text-xs">
                 1-minute quiz for personalized Insights
               </p>
             </div>
             <div className="order-1 md:order-2 space-y-3">
-              <p className="text-xs font-medium text-[#ABABAB] text-center md:text-start">
+              <p className="text-xs font-medium text-[#ABABAB] text-center md:text-start max-w-[21rem] md:max-w-full mx-auto">
                 By clicking "Get Started", you agree with Terms and Conditions,
                 Privacy Policy, Subscription Terms
               </p>
-              <p className={`block md:hidden ${nunito.className} text-[10px] font-medium text-[#ABABAB] text-center`}>
+              <p
+                className={`block md:hidden ${nunito.className} text-[10px] font-medium text-[#ABABAB] text-center`}
+              >
                 Fametonic 2025 ©All Rights Reserved.
               </p>
-              <p className={`hidden md:block ${urbanist.className} text-[10px] font-medium text-[#ABABAB] text-start`}>
+              <p
+                className={`hidden md:block ${urbanist.className} text-[10px] font-medium text-[#ABABAB] text-start`}
+              >
                 Fametonic 2025 ©All Rights Reserved.
               </p>
             </div>
@@ -71,7 +81,6 @@ const HomePage = () => {
           />
         </div>
       </div>
-      <div className="h-[500px]"></div>
     </>
   );
 };
