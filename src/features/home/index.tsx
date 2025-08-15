44d3d356921c1c910202469690c25b1962e85fd9
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-15 11:02:56 +0600

    changed image and added padding

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -5,35 +5,44 @@ import Image from "next/image";
 const HomePage = () => {
   return (
     <>
-    <div className="container grid grid-cols-1 md:grid-cols-2 items-center">
-      <div className="order-2 md:order-1">
-        <h2
-          className={`${urbanist.className} text-2xl md:text-4xl font-extrabold md:font-bold text-center md:text-start`}
-        >
-          Want to Turn Social Media Into a Profitable Career?
-        </h2>
-        <p
-          style={{
-            color: "#00E7F9",
-            textShadow: "0px 4px 4px #FC004E",
-          }}
-          className={`${urbanist.className} text-4xl font-bold text-center md:text-start`}
-        >
-          Discover your way to success with Fametonic:
-        </p>
+      <div className="container grid grid-cols-1 md:grid-cols-2 items-center">
+        <div className="order-2 md:order-1">
+          <div className="px-2 md:px-0">
+            <h2
+              className={`${urbanist.className} text-2xl md:text-4xl font-extrabold md:font-bold text-center md:text-start`}
+            >
+              Want to Turn Social Media Into a Profitable Career?
+            </h2>
+            <p
+              style={{
+                color: "#00E7F9",
+                textShadow: "0px 4px 4px #FC004E",
+              }}
+              className={`${urbanist.className} text-2xl md:text-4xl font-extrabold md:font-bold text-center md:text-start`}
+            >
+              Discover your way to success with Fametonic:
+            </p>
+          </div>
+          <div>
+            <p>✨ Start growing your influence right away—no waiting required!</p>
+            <p>✨ Start growing your influence right away—no waiting required!</p>
+            <p>✨ Start growing your influence right away—no waiting required!</p>
+            <p>✨ Start growing your influence right away—no waiting required!</p>
+            
+          </div>
+        </div>
+        <div className="order-1 md:order-2 px-4 md:px-0">
+          <Image
+            src="/images/hero_img.png"
+            alt="hero_img"
+            width={666}
+            height={679}
+            // 376px
+            className="w-full aspect-[12/11] object-cover"
+          />
+        </div>
       </div>
-      <div className="order-1 md:order-2">
-        <Image
-          src="/images/hero_img_sm.png"
-          alt="hero_img_sm"
-          width={666} // intrinsic size
-          height={679} // intrinsic size
-          // 376px
-          className="w-full aspect-[12/11] object-cover"
-        />
-      </div>
-    </div>
-    <div className="h-[500px]"></div>
+      <div className="h-[500px]"></div>
     </>
   );
 };
