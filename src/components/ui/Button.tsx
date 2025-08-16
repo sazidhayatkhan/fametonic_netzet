ca35b547e2896a59b28a4105c4ff16c050a619e0
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-17 00:52:40 +0600

    code refactored

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,30 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  iconSrc?: string;
+  iconAlt?: string;
+  className?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({
+  children,
+  iconSrc,
+  iconAlt = "icon",
+  className = "",
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`bg-[#FC004E] text-white text-xl font-bold h-[40px] w-full flex justify-center items-center gap-2 rounded-[10px] cursor-pointer shadow-[2px_2px_10px_0px_#00E7F9] ${className}`}
+      {...props}
+    >
+      {children}
+      {iconSrc && (
+        <span>
+          <img src={iconSrc} alt={iconAlt} className="w-[13px] h-[14px]" />
+        </span>
+      )}
+    </button>
+  );
+}
