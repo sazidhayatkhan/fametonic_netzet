077b64306123484a7397371cd25f7c0d29d496e7
sazidhayatkhan <sazidhayatkhan@gmail.com>
2025-08-17 11:53:11 +0600

    optimized classes

--- src/features/home/components/HeroSection.tsx
+++ src/features/home/components/HeroSection.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { urbanist } from "@/fonts/fonts";
+import clsx from "clsx";
+
+const baseTextStyles = clsx(
+  urbanist.className,
+  "text-2xl md:text-4xl leading-[30px] md:leading-10 font-extrabold md:font-bold",
+  "text-center md:text-start",
+  "max-w-[21rem] md:max-w-full mx-auto md:mx-0"
+);
 
 const HeroSection = () => {
   return (
     <section>
-      <h2
-        className={`${urbanist.className} text-2xl md:text-4xl leading-[30px] md:leading-10 font-extrabold md:font-bold text-center md:text-start max-w-[19rem] md:max-w-full mx-auto md:mx-0`}
-      >
+      <h2 className={clsx(baseTextStyles, "max-w-[19rem]")}>
         Want to Turn Social Media Into a Profitable Career?
       </h2>
       <p
@@ -14,7 +20,7 @@ const HeroSection = () => {
           color: "#00E7F9",
           textShadow: "0px 4px 4px #FC004E",
         }}
-        className={`${urbanist.className} text-2xl md:text-4xl leading-[30px] md:leading-10 font-extrabold md:font-bold text-center md:text-start max-w-[21rem] md:max-w-full mx-auto md:mx-0`}
+        className={baseTextStyles}
       >
         Discover your way to success with Fametonic:
       </p>
